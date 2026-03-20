7743603a0fd989996b18e7097d47b936d24f7773
Pushpendra Jaat <pushpendrajaat2001@gmail.com>
2026-03-20 19:06:42 +0530

    add sitemap function to define application URLs

---
+++ app/sitemap.ts
@@ -0,0 +1,9 @@
+export default function sitemap() {
+  return [
+    { url: 'https://mp-iti-to-rank-predictor.vercel.app/' },
+    { url: 'https://mp-iti-to-rank-predictor.vercel.app/submit' },
+    { url: 'https://mp-iti-to-rank-predictor.vercel.app/leaderboard' },
+    { url: 'https://mp-iti-to-rank-predictor.vercel.app/merit' },
+    { url: 'https://mp-iti-to-rank-predictor.vercel.app/my-rank' },
+  ]
+}
